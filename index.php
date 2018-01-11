84eac1434ecc0de8c29589004a7937f6a283fdd3
Sarah Elkhart <selkhart@mail.greenriver.edu>
2018-01-11 17:08:17 -0500

    changed to fat free

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
+<?php
 
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+
+
+//define a default route
+$f3->route('GET /',function() {
+    echo '<h1>Hello, world!</h1>';
+}
+);
+
+//run fat free
+$f3->run();
+?>
 
 <h1>hello</h1>
 <h2>How are you?</h2>
